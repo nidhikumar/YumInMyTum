c5db6dee47aca1bcca8747c29afa9f8c1b7ec849
Nidhi Kumar <nidhikr1401@gmail.com>
2024-03-10 14:00:04 -0700

    added the functionality of getting a new random dish

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import json
+import random
 
 app = Flask(__name__)
 
+with open('json/fastfood.json') as json_file: 
+    menu = json.load(json_file)
+
 @app.route('/')
 def index():
-    return 'Hello world!!'
+    return render_template('index.html')
+
+@app.route('/get-random-dish', methods=['POST'])
+def get_random_dish():
+    random_restaurant = random.choice(menu)
+    random_dish = random.choice(random_restaurant['foodItems'])
+    return jsonify(random_dish)
 
 if __name__ == "__main__":
     app.run(debug=True)
